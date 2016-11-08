cefbb4868a0439d671001c14abe25297c4a43254
Jason Kridner <jdk@ti.com>
2016-11-08 13:48:37 -0500

    Hack png2fb.py to try 32-bit mode

--- images/png2fb.py
+++ images/png2fb.py
@@ -5,17 +5,21 @@
 import sys
 from PIL import Image
 
-if len(sys.argv) > 2:
+if len(sys.argv) == 4:
     # print "\nReading: " + sys.argv[1]
     out = open(sys.argv[2], "wb")
+    mode = sys.argv[3]
+elif len(sys.argv) == 3:
+    out = open(sys.argv[2], "wb")
+    mode = "-16"
 elif len(sys.argv) == 2:
     out = sys.stdout
+    mode = "-16"
 else:
     print "Usage: png2fb.py infile [outfile [-32]]"
     sys.exit(1)
 
 im = Image.open(sys.argv[1])
-mode = sys.argv[2]
 
 if im.mode == "RGB":
     pixelSize = 3
@@ -27,17 +31,20 @@ else:
 pixels = im.tobytes()
 pixels2 = ""
 if mode == "-32":
+    print "Writing in 32-bit mode"
     for i in range(0, len(pixels) - 1, pixelSize):
         pixels2 += chr(ord(pixels[i]))
         pixels2 += chr(ord(pixels[i + 1]))
         pixels2 += chr(ord(pixels[i + 2]))
         pixels2 += chr(0)
 elif mode == "-24":
+    print "Writing in 24-bit mode"
     for i in range(0, len(pixels) - 1, pixelSize):
         pixels2 += chr(ord(pixels[i]))
         pixels2 += chr(ord(pixels[i + 1]))
         pixels2 += chr(ord(pixels[i + 2]))
 else:
+    print "Writing in 16-bit mode"
     for i in range(0, len(pixels) - 1, pixelSize):
         pixels2 += chr(ord(pixels[i + 2]) >> 3 | (ord(pixels[i + 1]) << 3 & 0xe0))
         pixels2 += chr(ord(pixels[i]) & 0xf8 | (ord(pixels[i + 1]) >> 5 & 0x07))
