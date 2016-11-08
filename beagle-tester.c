9b60feb8337a22928b56aa544020ee01e45fd7f2
root <root@beagleboard.localdomain>
2016-11-08 19:00:36 +0000

    Add 1280x1024 mode for xM

--- beagle-tester.c
+++ beagle-tester.c
@@ -3407,7 +3407,9 @@ void do_colorbar()
 	int x, y;
 
 	if (!init) {
-		if (fb_info.var.xres == 480)
+		if (fb_info.var.xres == 1280 || fb_info.var.bits_per_pixel == 32)
+			system("xzcat /usr/share/beagle-tester/itu-r-bt1729-colorbar-1280x1024-32.raw.xz > /dev/fb0");
+		else if (fb_info.var.xres == 480)
 			system("xzcat /usr/share/beagle-tester/itu-r-bt1729-colorbar-480x272.raw.xz > /dev/fb0");
 		else if (fb_info.var.xres == 800)
 			system("xzcat /usr/share/beagle-tester/itu-r-bt1729-colorbar-800x600.raw.xz > /dev/fb0");
