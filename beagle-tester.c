5e804cfe4c007d8ecb083fffa6bfe3ea46e18a8f
Robert Nelson <robertcnelson@gmail.com>
2017-01-10 15:56:30 -0600

    usb0: use 192.168.7.2 for usb0, as ping is failing to figure it out on its own
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- beagle-tester.c
+++ beagle-tester.c
@@ -3382,7 +3382,7 @@ void beagle_test(const char *scan_value)
 		beagle_notice("ethernet", r ? "fail" : "pass");
 	}
 
-	sprintf(str, "ping -s 8184 -i 0.01 -q -c 150 -w 2 -I usb0 192.168.7.1");
+	sprintf(str, "ping -s 8184 -i 0.01 -q -c 150 -w 2 -I 192.168.7.2 192.168.7.1");
 	fprintf(stderr, str);
 	fprintf(stderr, "\n");
 	fflush(stderr);
