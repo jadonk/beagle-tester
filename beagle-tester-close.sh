c195735069f6fc651af5cc2ac7405dc3c4fb93cd
Jason Kridner <jdk@ti.com>
2017-01-16 18:19:57 +0000

    Force remove fbtft kernel modules

--- beagle-tester-close.sh
+++ beagle-tester-close.sh
@@ -2,9 +2,9 @@
 board=$(cat /proc/device-tree/model | sed "s/ /_/g")
 case "${board}" in
 TI_AM335x_BeagleBone_Blue)
-	rmmod fbtft_device
-	rmmod fb_ili9341
-	rmmod fbtft
+	rmmod --force fbtft_device
+	rmmod --force fb_ili9341
+	rmmod --force fbtft
 	;;
 *)
 	systemctl restart getty@tty1.service
