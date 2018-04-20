5bf178504970a1add27a351107bc8ab92254eb62
Jason Kridner <jdk@ti.com>
2018-04-20 13:54:44 -0400

    Add Load cape

--- beagle-tester.c
+++ beagle-tester.c
@@ -7,7 +7,7 @@
  * Copyright (c) 1999-2000 Vojtech Pavlik
  * Copyright (c) 2009-2011 Red Hat, Inc
  * Copyright (C) 2009-2012 Tomi Valkeinen
- * Copyright (C) 2016 Texas Instruments
+ * Copyright (C) 2016-2018 Texas Instruments
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License version 2 as published by
@@ -3737,8 +3737,50 @@ int test_display70_cape(const char *scan_value, unsigned id)
 	return(fail);
 }
 
+/* BC0400A2yywwnnnnnnnn */
 int test_load_cape(const char *scan_value, unsigned id)
 {
+	int r;
+	int fd_sn;
+	char str[90];
+	char str2[90];
+
+	install_overlay(scan_value, capes[id].id_str);
+
+	fd_sn = open("/sys/bus/i2c/devices/i2c-2/2-0054/2-00540/nvmem", O_RDWR);
+	lseek(fd_sn, 0, SEEK_SET);
+	r = read(fd_sn, str, 88);
+	str[89] = 0;
+	beagle_notice("name", &str[6]);
+
+	gpio_out_test("sink1", 7);
+	gpio_out_test("sink2", 20);
+	gpio_out_test("sinkX", 115);
+	gpio_out_test("sinkX", 112);
+	gpio_out_test("sinkX", 45);
+	gpio_out_test("sinkX", 47);
+	gpio_out_test("sinkX", 27);
+
+	memcpy(str, cape_eeprom, 88);
+	strcpy(&str[6], capes[id].name);	/* board name */
+	memcpy(&str[38], &scan_value[4], 4);	/* board version */
+	strcpy(&str[58], capes[id].id_str);	/* part number */
+	strncpy(&str[76], &scan_value[8], 16);	/* serial number */
+	str[89] = 0;
+	lseek(fd_sn, 0, SEEK_SET);
+	r = write(fd_sn, str, 88);
+	lseek(fd_sn, 0, SEEK_SET);
+	r = read(fd_sn, str2, 88);
+	str2[89] = 0;
+	beagle_notice("name", &str2[6]);
+	beagle_notice("ver/mfr", &str2[38]);
+	beagle_notice("partno", &str2[58]);
+	beagle_notice("serial", &str2[76]);
+	fail = memcmp(str, str2, 88) ? 1 : 0;
+	beagle_notice("eeprom", fail ? "fail" : "pass");
+
+	close(fd_sn);
+
 	return(fail);
 }
 
@@ -3773,35 +3815,10 @@ int test_relay_cape(const char *scan_value, unsigned id)
 	str[89] = 0;
 	beagle_notice("name", &str[6]);
 
-	system("echo out > /sys/class/gpio/gpio20/direction");
-	system("echo 1 > /sys/class/gpio/gpio20/value");
-	beagle_notice("relay1", "on");
-	system("sleep 1");
-	system("echo 0 > /sys/class/gpio/gpio20/value");
-	beagle_notice("relay1", "off");
-	system("sleep 1");
-	system("echo out > /sys/class/gpio/gpio7/direction");
-	system("echo 1 > /sys/class/gpio/gpio7/value");
-	beagle_notice("relay2", "on");
-	system("sleep 1");
-	system("echo 0 > /sys/class/gpio/gpio7/value");
-	beagle_notice("relay2", "off");
-	system("sleep 1");
-	system("echo out > /sys/class/gpio/gpio112/direction");
-	system("echo out > /sys/class/gpio/gpio112/direction");
-	system("echo 1 > /sys/class/gpio/gpio112/value");
-	beagle_notice("relay3", "on");
-	system("sleep 1");
-	system("echo 0 > /sys/class/gpio/gpio112/value");
-	beagle_notice("relay3", "off");
-	system("sleep 1");
-	system("echo out > /sys/class/gpio/gpio115/direction");
-	system("echo out > /sys/class/gpio/gpio115/direction");
-	system("echo 1 > /sys/class/gpio/gpio115/value");
-	beagle_notice("relay4", "on");
-	system("sleep 1");
-	system("echo 0 > /sys/class/gpio/gpio115/value");
-	beagle_notice("relay4", "off");
+	gpio_out_test("relay1", 20);
+	gpio_out_test("relay2", 7);
+	gpio_out_test("relay3", 112);
+	gpio_out_test("relay4", 115);
 
 	//for(r = 0; r < 88; r++) printf("%02x", cape_eeprom[r]); printf("\n");
 	memcpy(str, cape_eeprom, 88);
@@ -3849,4 +3866,26 @@ void install_overlay(const char *scan_value, const char *id_str)
 	/* #dtb_overlay=/lib/firmware/<file8>.dtbo */
 	const char *cmd = "perl -i.bak -pe 's!^.*dtb_overlay=/lib/firmware/.+\.dtbo.*!dtb_overlay=/lib/firmware/%s.dtbo!;' /boot/uEnv.txt";
 	printf(cmd, id_str);
+}
+
+int gpio_out_test(const char *name, unsigned pin)
+{
+	const char *sleep = "sleep 1";
+	const char *dir_out = "echo out > /sys/class/gpio/gpio%i/direction";
+	const char *value_high = "echo 1 > /sys/class/gpio/gpio%i/value";
+	const char *value_low = "echo 0 > /sys/class/gpio/gpio%i/value";
+	char buffer[50];
+
+	sprintf(buffer, dir_out, pin);
+	system(buffer);
+	sprintf(buffer, value_high, pin);
+	system(buffer);
+	beagle_notice(name, "on");
+	system(sleep);
+	sprintf(buffer, value_low, pin);
+	system(buffer);
+	beagle_notice(name, "off");
+	system(sleep);
+
+	return(0);
 }
